a3c04a75c96933156f1409c64654a22cc108cbd3
Viktory89 <economist.prof24@gmail.com>
2025-02-04 15:35:09 +0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -6,7 +6,7 @@
       "author": "Твое имя",
       "name": "Заметки",
       "description": "Простой виджет для заметок в AmoCRM",
-      "base_url": "https://ТВОЙ-РЕНДЕР-ДОМЕН/", 
+      "base_url": "https://amo-notes-widget.onrender.com/", 
       "locations": {
         "lcard": {
           "url": "widget.js"
