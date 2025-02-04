86989d4d3f899c5f976c4417a582defafd12993a
Viktory89 <economist.prof24@gmail.com>
2025-02-04 15:14:16 +0300

    Create widget.js

---
+++ widget.js
@@ -0,0 +1,17 @@
+{
+  "widgets": [
+    {
+      "id": "notes_widget",
+      "version": "1.0.0",
+      "author": "Твое имя",
+      "name": "Заметки",
+      "description": "Простой виджет для заметок в AmoCRM",
+      "base_url": "https://ТВОЙ-РЕНДЕР-ДОМЕН/", 
+      "locations": {
+        "lcard": {
+          "url": "widget.js"
+        }
+      }
+    }
+  ]
+}
